2c81c57fcbd802ec3e65d5486dfc12685d009782
HM-Wimalasiri <harshanmadhurangaw@gmail.com>
2024-01-15 13:17:34 +0530

    Added Grid view for category screen

--- lib/categories.dart
+++ lib/categories.dart
@@ -8,10 +8,24 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:const Text("Pick your category"),
-      ),
-      body: GridView(children: [],)
-    );
+        appBar: AppBar(
+          title: const Text("Pick your category"),
+        ),
+        body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: const [
+            Text("1"),
+            Text("2"),
+            Text("3"),
+            Text("4"),
+            Text("5"),
+            Text("6"),
+          ],
+        ));
   }
 }
