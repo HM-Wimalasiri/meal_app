d905bb8bfa52782ca271158bdce0c6cca175319b
HM-Wimalasiri <harshanmadhurangaw@gmail.com>
2024-01-22 22:48:33 +0530

    Added category menu

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -2,10 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:mealapp/data/dummy_data.dart';
 import 'package:mealapp/main.dart';
+import 'package:mealapp/models/category.dart';
+import 'package:mealapp/screens/meals.dart';
 import 'package:mealapp/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) =>
+            MealsScreen(title: category.title, meals: filteredMeals)));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +34,12 @@ class CategoriesScreen extends StatelessWidget {
           ),
           children: [
             for (final category in availableCategories)
-              CategoryGridItem(category: category)
+              CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context, category);
+                },
+              )
           ],
         ));
   }
