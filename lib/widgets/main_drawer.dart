c47f5e8ff4801e5cf35c2fd79fc00ca56eb2b351
HM-Wimalasiri <harshanmadhurangaw@gmail.com>
2024-01-31 00:46:49 +0530

    Implemented filters screen

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({super.key, required this.onSelectScreen});
+
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
@@ -50,7 +52,9 @@ class MainDrawer extends StatelessWidget {
           style: Theme.of(context).textTheme.titleSmall!.copyWith(
               color: Theme.of(context).colorScheme.onBackground, fontSize: 24),
         ),
-        onTap: () {},
+        onTap: () {
+          onSelectScreen('meals');
+        },
       ),
       ListTile(
         leading: Icon(
@@ -63,7 +67,9 @@ class MainDrawer extends StatelessWidget {
           style: Theme.of(context).textTheme.titleSmall!.copyWith(
               color: Theme.of(context).colorScheme.onBackground, fontSize: 24),
         ),
-        onTap: () {},
+        onTap: () {
+          onSelectScreen('filters');
+        },
       )
     ]));
   }
